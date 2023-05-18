f38ec7a3cdca4e615a5790a86cb15fbd4144c85b
ThiagoBrunoPereira <thiagomeezz@gmail.com>
2023-05-18 17:19:51 -0300

    chore(welcome): custom home, implement form

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body>
+    @if(session()->has('message'))
+    <div class="bg-green-50 p-4 absolute top-0 left-0 z-10 w-full">
+        <div class="flex">
+            <div class="flex-shrink-0">
+                <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                </svg>
+            </div>
+            <div class="ml-3">
+                <h3 class="text-sm font-medium text-green-800">{{ session()->get('message') }}</h3>
+                <div class="mt-2 text-sm text-green-700">
+                    <p>Em processamento...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+
+
+    <div class="relative isolate overflow-hidden bg-gray-900">
+        <div class="w-screen h-screen flex justify-center items-center">
+            <form action="/enviar" method="post">
+                @csrf
+                <button type="submit" class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Enviar para fila</button>
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
