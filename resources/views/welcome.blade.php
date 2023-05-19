2789bbb0475439718ae867b4b15952fa0ac8fc95
ThiagoBrunoPereira <thiagomeezz@gmail.com>
2023-05-19 11:05:44 -0300

    feat(welcome): ajust align alert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
 <body>
     @if(session()->has('message'))
     <div class="bg-green-50 p-4 absolute top-0 left-0 z-10 w-full">
-        <div class="flex">
+        <div class="flex justify-center">
             <div class="flex-shrink-0">
                 <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
