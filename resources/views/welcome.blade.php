0255d3864c1bb7e4ab016a127d33724ccb8d3b73
ThiagoBrunoPereira <thiagomeezz@gmail.com>
2023-05-19 10:56:09 -0300

    chore(welcome): create input file, custom form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,16 +33,26 @@
     </div>
     @endif
 
-
-    <div class="relative isolate overflow-hidden bg-gray-900">
-        <div class="w-screen h-screen flex justify-center items-center">
-            <form action="/enviar" method="post">
+    <div class="w-screen h-screen flex justify-center items-center">
+        <div class="border-b border-gray-900/10 pb-12">
+            <h2 class="text-base font-semibold leading-7 text-gray-900">Importação</h2>
+            <p class="mt-1 text-sm leading-6 text-gray-600">Após o envio, os dados serão enviados a uma fila de cadastramento.</p>
+            <form action="/enviar" method="post" enctype="multipart/form-data">
                 @csrf
-                <button type="submit" class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Enviar para fila</button>
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="file" class="block text-sm font-medium leading-6 text-gray-900">Documento</label>
+                        <div class="mt-2">
+                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input type="file" name="document" id="document" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 focus:ring-0 sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Enviar para fila</button>
+                </div>
             </form>
         </div>
     </div>
-
 </body>
 
 </html>
